2ca511a76d80dffc2bd4c4aba07bbf43924a0ec9
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2022-08-09 15:48:40 +0530

    Updated Head Nav Link

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
 		let header_element = document.getElementById("header");
 		let header_text = `
         <div class="title">
-	<h1>Portfolio</h1>
+	<h1><a href="https://vinnyab28.github.io/portfolio" style="text-decoration: none">Portfolio</a></h1>
 </div>
 <div class="social-links">
-	<a href="#">Github</a>
-	<a href="#">LinkedIn</a>
+	<a href="https://github.com/vinnyab28" target="_blank">Github</a>
+	<a href="https://in.linkedin.com/in/vineeth-prakash" target="_blank">LinkedIn</a>
 </div>`;
 		header_element.innerHTML = header_text;
 		return;
